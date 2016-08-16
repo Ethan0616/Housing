dfbf70f423b761d2b27213dc4c7acb00c624e033
Ethan <dfhwangqiang@163.com>
2016-08-16 16:11:05 +0800

    添加UIImage扩展，增加模糊效果

--- Housing/Body/Categorys/UIKit/Extension_UIImage.swift
+++ Housing/Body/Categorys/UIKit/Extension_UIImage.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import Accelerate
+
 
 // UIImage的扩展
 extension UIImage {
@@ -68,6 +70,84 @@ extension UIImage {
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
+    }
+    
+    /// 将传入的图片，生成对应的模糊效果
+    func boxblurImageWithBlur(blur : CGFloat) -> UIImage{
+        let imageData : NSData = UIImageJPEGRepresentation(self, 1)!
+        let destImage : UIImage = UIImage(data:imageData)!
+        var blur = blur
+        if blur < 0 || blur > 1 {
+            blur = 0.5
+        }
+        var boxSize : Int = Int(blur * 40.0)
+        boxSize = boxSize - (boxSize % 2) + 1
+        
+        let img : CGImageRef = destImage.CGImage!
+        var inBuffer , outBuffer , outBuffer2 : vImage_Buffer
+        var pixelBuffer , pixelBuffer2 :  UnsafeMutablePointer<Void>
+        var error : vImage_Error?
+        
+        let imgHeight  = UInt(CGImageGetHeight(img))
+        let imgWidth  = UInt(CGImageGetWidth(img))
+        let rowBytes  = CGImageGetBytesPerRow(img)
+        
+        
+        //create vImage_Buffer with data from CGImageRef
+        
+        let inProvider : CGDataProviderRef = CGImageGetDataProvider(img)!
+        let inBitmapData : CFDataRef = CGDataProviderCopyData(inProvider)!
+        
+        inBuffer = vImage_Buffer(data:UnsafeMutablePointer(CFDataGetBytePtr(inBitmapData)), height: imgHeight, width: imgWidth, rowBytes: rowBytes)
+        
+        
+        //create vImage_Buffer for output
+
+        pixelBuffer = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img))
+        
+        outBuffer = vImage_Buffer(data: pixelBuffer, height: imgHeight, width: imgWidth, rowBytes: rowBytes)
+        
+        // Create a third buffer for intermediate processing
+        pixelBuffer2 = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img))
+        
+        outBuffer2 = vImage_Buffer(data: pixelBuffer2, height: imgHeight, width: imgWidth, rowBytes: rowBytes)
+        
+        error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer2, nil, 0, 0, UInt32(boxSize), UInt32(boxSize), nil, UInt32(kvImageEdgeExtend));
+
+
+        if let err = error {
+            print("error from convolution \(err)")
+        }
+        error = vImageBoxConvolve_ARGB8888(&outBuffer2, &inBuffer, nil, 0, 0, UInt32(boxSize), UInt32(boxSize), nil, UInt32(kvImageEdgeExtend));
+
+        if let err = error {
+            print("error from convolution \(err)")
+        }
+        error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, nil, 0, 0, UInt32(boxSize), UInt32(boxSize), nil, UInt32(kvImageEdgeExtend));
+
+        if let err = error {
+            print("error from convolution \(err)")
+        }
+        
+        
+        let  colorSpace  = CGColorSpaceCreateDeviceRGB()!
+        // data: UnsafeMutablePointer<Void>, _ width: Int, _ height: Int, _ bitsPerComponent: Int, _ bytesPerRow: Int, _ space: CGColorSpace?, _ bitmapInfo: UInt32
+        let skipLast : UInt32 = CGImageAlphaInfo.NoneSkipLast.rawValue
+        let  ctx  = CGBitmapContextCreate(outBuffer.data,
+                                                 Int(outBuffer.width),
+                                                 Int(outBuffer.height),
+                                                 8,
+                                                 outBuffer.rowBytes,
+                                                 colorSpace,
+                                                 skipLast)
+        let imageRef = CGBitmapContextCreateImage(ctx)
+        let returnImage = UIImage(CGImage: imageRef!)
+        
+        //clean up
+        free(pixelBuffer);
+        free(pixelBuffer2);
+        
+        return returnImage
     }
     
 }
