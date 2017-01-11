7c1161827bceb207f3532d6060847769124c69f4
Ethan <dfhwangqiang@163.com>
2017-01-11 16:32:44 +0800

    .

--- Housing/Body/Extension/UIKit/Extension_UIViewController.swift
+++ Housing/Body/Extension/UIKit/Extension_UIViewController.swift
@@ -34,95 +34,47 @@ public extension DispatchQueue {
 
 
 extension UIViewController {
-    open override class func initialize() {
-        
-        // make sure this isn't a subclass
-        if self !== UIViewController.self {
-            return
-        }
-        
-        DispatchQueue.once(token: "swizzledMethod\(description)", block:{
-
-            
-            let originalSelector = #selector(UIViewController.viewWillAppear(_:))
-            let swizzledSelector = Selector(("nsh_viewWillAppear:"))
-            
-            let originalMethod = class_getInstanceMethod(self, originalSelector)
-            let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
-            
-            let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-            
-            if didAddMethod {
-                class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
-            } else {
-                method_exchangeImplementations(originalMethod, swizzledMethod);
-            }
-
-        
-
-        })
-    }
-    
-    // MARK: - Method Swizzling
-    
-    func nsh_viewWillAppear(animated: Bool) {
-        
-        nsh_viewWillAppear(animated: animated)
-        
-        if description.isEmpty {
-            print("viewWillAppear: \(self)")
-
-        }else{
-            print("viewWillAppear: \(description)")
-        }
-    }
+//    open override class func initialize() {
+//        
+//        // make sure this isn't a subclass
+//        if self !== UIViewController.self {
+//            return
+//        }
+//        
+//        DispatchQueue.once(token: "swizzledMethod\(description)", block:{
+//
+//            
+//            let originalSelector = #selector(UIViewController.viewWillAppear(_:))
+//            let swizzledSelector = Selector(("nsh_viewWillAppear:"))
+//            
+//            let originalMethod = class_getInstanceMethod(self, originalSelector)
+//            let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+//            
+//            let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+//            
+//            if didAddMethod {
+//                class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+//            } else {
+//                method_exchangeImplementations(originalMethod, swizzledMethod);
+//            }
+//
+//        
+//
+//        })
+//    }
+//    
+//    // MARK: - Method Swizzling
+//    
+//    func nsh_viewWillAppear(animated: Bool) {
+//        
+//        nsh_viewWillAppear(animated: animated)
+//        
+//        if description.isEmpty {
+//            print("viewWillAppear: \(self)")
+//
+//        }else{
+//            print("viewWillAppear: \(description)")
+//        }
+//    }
 }
 
-/*
- #import <objc/runtime.h>
- 
- @implementation UIViewController (Tracking)
- 
- + (void)load {
- static dispatch_once_t onceToken;
- dispatch_once(&onceToken, ^{
- Class class = [self class];
- 
- SEL originalSelector = @selector(viewWillAppear:);
- SEL swizzledSelector = @selector(xxx_viewWillAppear:);
- 
- Method originalMethod = class_getInstanceMethod(class, originalSelector);
- Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
- 
- // When swizzling a class method, use the following:
- // Class class = object_getClass((id)self);
- // ...
- // Method originalMethod = class_getClassMethod(class, originalSelector);
- // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
- 
- BOOL didAddMethod =
- class_addMethod(class,
- originalSelector,
- method_getImplementation(swizzledMethod),
- method_getTypeEncoding(swizzledMethod));
- 
- if (didAddMethod) {
- class_replaceMethod(class,
- swizzledSelector,
- method_getImplementation(originalMethod),
- method_getTypeEncoding(originalMethod));
- } else {
- method_exchangeImplementations(originalMethod, swizzledMethod);
- }
- });
- }
- 
- #pragma mark - Method Swizzling
- 
- - (void)xxx_viewWillAppear:(BOOL)animated {
- [self xxx_viewWillAppear:animated];
- NSLog(@"viewWillAppear: %@", self);
- }
- 
- @end
- */
